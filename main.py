57d517a62d5d695b3d964f407130fbb7ff68582a
RehaanAnjariaAgain <rehaananjariaagain1@MacBook-Air-2.local>
2024-08-28 15:37:58 -0400

    fixed cors

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ origins = [
     "http://127.0.0.1:8000",
     "http://localhost",
     "http://localhost:8080",
-    "https://www.bosdroneworks.com"
+    "https://crlspathfinders-frontend.vercel.app"
 ]
 
 app.add_middleware(
